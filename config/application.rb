62dffc8fabfb88f61abdd5b54607c374dce87628
sediqwe <sediqwe@gmail.com>
2023-02-19 21:14:04 +0100

    Games 3.0

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Monsterv2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.time_zone = 'Europe/Budapest'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :hu
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
