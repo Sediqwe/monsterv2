9c59ff1cdfcb6b83e4ed2d8ef2171ff5586645a1
sediqwe <sediqwe@gmail.com>
2023-03-04 17:56:13 +0100

    Magyaritasok.hu

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,15 +1,5 @@
 $(document).on('ready', function() {
-  const navbar = document.querySelector('.navbar')
-  navbar.querySelector('.toggle').addEventListener('click',()=>
-  {
-  navbar.classList.toggle('collapsed')
-  });
-  window.addEventListener('scroll',(e)=>{
-  let windowY = window.pageYOffset
-  let navbarHeight = document.querySelector('.navbar').offsetHeight
-  if(windowY>navbarHeight) navbar.classList.add('sticky')
-  else navbar.classList.remove('sticky')
-  });
+  
     $('#sidebarCollapse').on('click', function() {
         $('#sidebar, #content').toggleClass('active');
       });
