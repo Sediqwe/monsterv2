27df19b8b2fa5e16db585f796040c37e4e62dc7a
sediqwe <sediqwe@gmail.com>
2023-07-08 01:15:10 +0200

    Koala integritás

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,15 +1,7 @@
 Koala.configure do |config|
-  config.access_token = 'MY_TOKEN'
-  config.app_access_token = Koala.configure do |config|
-  config.access_token = 'MY_TOKEN'
-  config.app_access_token = 'MY_APP_ACCESS_TOKEN'
-  config.app_id = 'MY_APP_ID'
-  config.app_secret = 'MY_APP_SECRET'
+  config.access_token = Rails.application.credentials.facebook[:access_token]
+  config.app_access_token = Rails.application.credentials.facebook[:app_access_token]
+  config.app_id = Rails.application.credentials.facebook[:app_id]
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
-    config.app_id = 'MY_APP_ID'
-    config.app_secret = 'MY_APP_SECRET'
-    # See Koala::Configuration for more options, including details on how to send requests through
-    # your own proxy servers.
-  end
