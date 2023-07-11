164a7fc446170effbc7ff6a90ffb3269e73e0d39
sediqwe <sediqwe@gmail.com>
2023-07-11 16:50:03 +0200

    games STEAM ellenörzés 2a

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,6 +1,6 @@
 class FacebookController < ApplicationController
   def index
-      @facebook = Upload.order('id DESC').limit(20)
+      @facebook = Upload.order('id DESC').limit(200)
     
    
   end
