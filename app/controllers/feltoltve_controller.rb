e8090a29490bda5ca1485c5153edc98079b25365
sediqwe <sediqwe@gmail.com>
2023-08-04 15:08:01 +0200

    Fix Feltoltes

--- app/controllers/feltoltve_controller.rb
+++ app/controllers/feltoltve_controller.rb
@@ -4,10 +4,20 @@ class FeltoltveController < ApplicationController
        adat = Upload.where(special: [false, nil]).first
       if adat
           game = Game.find(adat.game_id)
-          translater = Translater.find(adat.translater_id)
+          translater = Uploadtranslater.where(upload_id: adat.id)
+          trn = ""
+          p translater.inspect
+          translater.each_with_index do |dorka, index|
+            trn_name = Translater.find(dorka.translater_id)
+            if index < translater.size-1
+              trn += trn_name.translater_name + ", "
+            else
+              trn +=  trn_name.translater_name  
+            end
+          end
           adat.special = true
           adat.save
-          render html: (adat.name + "||||Ł" + adat.version + "||||Ł" + game.slug + "||||Ł" + translater.translater_name  )
+          render html: (adat.name + "||||Ł" + adat.version + "||||Ł" + game.slug + "||||Ł" + trn  )
       else
       render html: ("")
       end
