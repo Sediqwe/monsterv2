6e2bdd87661a074fc1e776c0246e50ca6ab41d41
sediqwe <sediqwe@gmail.com>
2023-10-07 12:31:09 +0200

    Translater berak

--- app/helpers/translaters_helper.rb
+++ app/helpers/translaters_helper.rb
@@ -3,4 +3,19 @@ module TranslatersHelper
     def forditasok(id)
         feltoltesek = Upload.where(translater_id: id).order(id: :DESC).first(10)
     end
+    def transtop(id)
+        translater_id = id
+        ingwer = ""
+        intem = 1
+        top_downloads = Download.group(:upload_id).count.sort_by { |_, count| -count }
+        top_downloads.each do |upload_id, count|
+          if Uploadtranslater.exists?(upload_id: upload_id, translater_id: translater_id)
+            ingwer += "#{intem}. #{Upload.find(upload_id).game.name} (#{count}) <br>"
+            intem += 1
+            break if intem > 10  # Kilépés a ciklusból, ha intem értéke 10
+          end
+        end
+        ingwer
+      end
+
 end
