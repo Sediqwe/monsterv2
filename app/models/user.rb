b579000eb119be114ee89c4f8f9b6f0aa1a61ccc
sediqwe <sediqwe@gmail.com>
2023-10-09 19:33:57 +0200

    Támogatás vissza Shuffle

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     before_save { self.name = name.downcase }
     validates :email, uniqueness: true, presence: true, length: {minimum: 5, maximum:100}
-    validates :name, uniqueness: true, presence: true, length: {minimum: 5, maximum:100}
+    validates :name, uniqueness: true, presence: true, length: {minimum: 2, maximum:100}
     has_many :game
     has_one_attached :photo , dependent: :delete_all
 end
