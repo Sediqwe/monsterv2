4b50e90a796a2bc543e410ac57efe7b78b11ab4c
sediqwe <sediqwe@gmail.com>
2023-11-10 21:17:23 +0100

    Games foglalás előkészületek

--- db/migrate/20231110171215_add_stipi_to_games.rb
+++ db/migrate/20231110171215_add_stipi_to_games.rb
@@ -3,7 +3,7 @@ class AddStipiToGames < ActiveRecord::Migration[7.0]
     add_column :games, :stipi, :boolean, default: false
     add_column :games, :hidden, :boolean, default: false
     add_column :games, :hatarido, :datetime
-    game.update(hidden: false)
-    game.update(stipi: false)
+    games.update(hidden: false)
+    games.update(stipi: false)
   end
 end
