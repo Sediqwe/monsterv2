4ada06df2b90f0954eb1a4be4f5fb462244297b6
sediqwe <sediqwe@gmail.com>
2023-12-23 16:58:56 +0100

    Támogatás átírva

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true, length: {minimum: 5, maximum:100}
     validates :name, uniqueness: true, presence: true, length: {minimum: 2, maximum:100}
     has_many :game
+    belongs_to :translater
     has_one_attached :photo , dependent: :delete_all
 end
