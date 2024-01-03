2c99b557e8283368306cbd7cbe9baf0ebe08efd7
sediqwe <sediqwe@gmail.com>
2024-01-03 16:32:42 +0100

    Email is uniq

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_01_172705) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_153135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -462,6 +462,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_01_172705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "user_id", default: 0
     t.index ["slug"], name: "index_translaters_on_slug", unique: true
   end
 
@@ -528,6 +529,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_01_172705) do
     t.string "tam4"
     t.bigint "translater_id"
     t.text "recovery"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
     t.index ["translater_id"], name: "index_users_on_translater_id"
   end
