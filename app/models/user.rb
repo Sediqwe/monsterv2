23b1d957b7e7206a34cbb92f22f6b93a0077e6b1
sediqwe <sediqwe@gmail.com>
2024-02-12 20:17:20 +0100

    User fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true, length: {minimum: 5, maximum:100}
     validates :name, uniqueness: true, presence: true, length: {minimum: 2, maximum:100}
     has_many :game
-    belongs_to :translater
+    has_one :translater
     has_one_attached :photo , dependent: :delete_all
     has_many :supportlists, foreign_key: "user_id"
 end
