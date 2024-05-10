82356739b40b0f04229d29be4ef77db1dcbd44fa
Sediqwe <sediqwe@gmail.com>
2024-05-10 19:59:01 +0200

    Fix

--- app/models/gamemessage.rb
+++ app/models/gamemessage.rb
@@ -4,7 +4,7 @@ class Gamemessage < ApplicationRecord
   def self.latest_messages_per_game
     # Első lépésként lekérjük az utolsó 10 üzenetet minden game_id-hez,
     # és rendezzük azokat időrendben csökkenő sorrendbe.
-    latest_messages = Gamemessage.order(created_at: :desc).limit(10)
+    latest_messages = Gamemessage.order(created_at: :desc).limit(100)
 
     # Majd csoportosítjuk az üzeneteket game_id szerint, és kiválasztjuk a legfrissebb üzenetet minden csoportból.
     latest_messages_per_game = latest_messages.group_by(&:game_id).map { |_, messages| messages.first }
