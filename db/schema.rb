8319b284cb6eba64d46fdf1bf51565c16b105b7e
Sediqwe <sediqwe@gmail.com>
2024-05-16 19:16:24 +0200

    Game.Name.Uniq

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_07_165657) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_16_171005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -282,6 +282,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_07_165657) do
     t.datetime "hatarido"
     t.boolean "okes", default: false
     t.string "stipiusername"
+    t.index ["name"], name: "index_games_on_name", unique: true
     t.index ["slug"], name: "index_games_on_slug", unique: true
     t.index ["user_id"], name: "index_games_on_user_id"
   end
