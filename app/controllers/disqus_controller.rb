d5e712c8727208fd9df5d927d1454426a9b6e8f5
Sediqwe <sediqwe@gmail.com>
2024-06-03 18:54:22 +0200

    Egyedi

--- app/controllers/disqus_controller.rb
+++ app/controllers/disqus_controller.rb
@@ -1,5 +1,14 @@
 class DisqusController < ApplicationController
     def index
         @gamemessage = Gamemessage.where(accept: nil)
-        end
+        require 'date'
+
+        egy_honapja = Date.today << 1  # Egy hónappal ezelőtti dátum
+
+        @egyedi_beleptetesek = ActivityLog
+        .where("note LIKE 'Beléptetve:%'")
+        .where(created_at: egy_honapja..Date.today)
+        .distinct
+        .count
+    end
 end
