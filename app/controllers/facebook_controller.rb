e4907a016528047eaf6ad473c8b44732a50c5940
Sediqwe <sediqwe@gmail.com>
2024-06-16 22:35:56 +0200

    day

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,7 +1,7 @@
 class FacebookController < ApplicationController
   def index
-    start_of_week = Date.today.beginning_of_week
-    end_of_week = Date.today.end_of_week
+    start_of_week = Date.current.beginning_of_week
+    end_of_week = Date.current.end_of_week.end_of_day
     
     # Lekérdezés az aktuális hét feltöltéseihez
     @facebook = Upload.where(created_at: start_of_week..end_of_week).order('id DESC').limit(200)
